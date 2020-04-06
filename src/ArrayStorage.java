b00e7bccebdcb525c64cafe0354f3ae2cd7d1f29
RVohmin <vohmin77@gmail.com>
2020-04-06 21:18:44 +0300

    Home Task "Resume Storage" baseJava Домашнее задание HW2

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,42 +1,100 @@
+import java.util.Arrays;
+
 /**
- * Array based storage for Resumes
+ * Array based storage for Resumes.
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int count = 0;
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
 
-    void clear() {
-        for (int i = 0; i < count; i++) {
-            storage[i] = null;
-        }
-        count = 0;
-    }
-
-    void save(Resume resume) {
-        if (count < storage.length) {
-            storage[count] = resume;
-            count++;
-        }
-    }
-
-    Resume get(String uuid) {
-        for (int i = 0; i < count; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                return storage[i];
+    /**
+     * Method check for resume present in storage.
+     * @param uuid - String uuid.
+     * @return boolean true if present.
+     */
+    private boolean checkPresent(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                return true;
             }
+        }
+        return false;
+    }
+
+    /**
+     * clearing storage of resume.
+     */
+    void clear() {
+        Arrays.fill(storage, null);
+        size = 0;
+    }
+
+    /**
+     * Method replaced resume
+     * @param resume - new version resume.
+     */
+    public void update(Resume resume) {
+        if (checkPresent(resume.toString())) {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].equals(resume)) {
+                    storage[i] = resume;
+                }
+            }
+        } else {
+            System.out.println("Error: resume with such uuid absent");
+        }
+    }
+
+    /**
+     * Save new resume to storage.
+     * @param resume - new resume.
+     */
+    void save(Resume resume) {
+        if (!checkPresent(resume.toString())) {
+            if (size < storage.length) {
+                storage[size] = resume;
+                size++;
+            } else {
+                System.out.println("Error: Resume cannot be saved - storage is full");
+            }
+        } else {
+            System.out.println("Error: Such resume is exist, use update() method");
+        }
+    }
+
+    /**
+     * get resume from storage.
+     * @param uuid - uuid resume.
+     * @return - Resume if it exist in storage.
+     */
+    Resume get(String uuid) {
+        if (checkPresent(uuid)) {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].uuid.equals(uuid)) {
+                    return storage[i];
+                }
+            }
+        } else {
+            System.out.printf("Error: resume with uuid \"%s\" is absent in storage \n", uuid);
         }
         return null;
     }
 
+    /**
+     * delete resume from storage if it exist in storage.
+     * @param uuid - uuid resume.
+     */
     void delete(String uuid) {
-        int i = 0;
-        while (i < count) {
-            if (storage[i].toString().equals(uuid)) {
-                System.arraycopy(storage, i + 1, storage, i, count - i - 1);
-                count--;
-                break;
+        if (checkPresent(uuid)) {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].toString().equals(uuid)) {
+                    System.arraycopy(storage, i + 1, storage, i, size - i - 1);
+                    size--;
+                    break;
+                }
             }
-            i++;
+        } else {
+            System.out.println("Error: there isn't resume with such uuid");
         }
     }
 
@@ -44,16 +102,13 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] resumes = new Resume[count];
-        int i = 0;
-        while (i < count) {
-            resumes[i] = storage[i];
-            i++;
-        }
-        return resumes;
+        return Arrays.copyOf(storage, size);
     }
 
+    /**
+     * @return size of storage (quantity of resumes)
+     */
     int size() {
-        return count;
+        return size;
     }
 }
