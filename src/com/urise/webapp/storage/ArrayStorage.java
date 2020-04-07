8c5a0afb9d5b9df20159480ef261903e9c79d2f6
RVohmin <vohmin77@gmail.com>
2020-04-07 14:35:28 +0300

    Home Task "Resume Storage" baseJava Домашнее задание HW2 устраняем дублирование

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -8,42 +8,26 @@ import java.util.Arrays;
  * Array based storage for Resumes.
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10_000];
     private int size = 0;
-
-    /**
-     * Method check for resume present in storage.
-     * @param uuid - String uuid.
-     * @return boolean true if present.
-     */
-    private boolean checkPresent(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                return true;
-            }
-        }
-        return false;
-    }
 
     /**
      * clearing storage of resume.
      */
     public void clear() {
-        Arrays.fill(storage, null);
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
     /**
      * Method replaced resume
+     *
      * @param resume - new version resume.
      */
     public void update(Resume resume) {
-        if (checkPresent(resume.getUuid())) {
-            for (int i = 0; i < size; i++) {
-                if (storage[i].getUuid().equals(resume.getUuid())) {
-                    storage[i] = resume;
-                }
-            }
+        Resume temp = this.get(resume.getUuid());
+        if (temp != null) {
+            storage[Arrays.asList(storage).indexOf(temp)] = resume;
         } else {
             System.out.println("Error: resume with such uuid absent");
         }
@@ -51,52 +35,46 @@ public class ArrayStorage {
 
     /**
      * Save new resume to storage.
+     *
      * @param resume - new resume.
      */
     public void save(Resume resume) {
-        if (!checkPresent(resume.getUuid())) {
-            if (size < storage.length) {
-                storage[size] = resume;
-                size++;
-            } else {
-                System.out.println("Error: com.urise.webapp.model.Resume cannot be saved - storage is full");
-            }
+        if ((get(resume.getUuid()) == null) && (size < storage.length)) {
+            storage[size] = resume;
+            size++;
         } else {
-            System.out.println("Error: Such resume is exist, use update() method");
+            System.out.println("Error: Such resume is exist, use update() method, or storage is full");
         }
     }
 
     /**
      * get resume from storage.
+     *
      * @param uuid - uuid resume.
+     *
      * @return - com.urise.webapp.model.Resume if it exist in storage.
      */
     public Resume get(String uuid) {
-        if (checkPresent(uuid)) {
-            for (int i = 0; i < size; i++) {
-                if (storage[i].getUuid().equals(uuid)) {
-                    return storage[i];
-                }
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return storage[i];
             }
-        } else {
-            System.out.printf("Error: resume with uuid \"%s\" is absent in storage \n", uuid);
         }
+        System.out.printf("Error: resume with uuid \"%s\" is absent in storage \n", uuid);
         return null;
     }
 
     /**
      * delete resume from storage if it exist in storage.
+     *
      * @param uuid - uuid resume.
      */
     public void delete(String uuid) {
-        if (checkPresent(uuid)) {
-            for (int i = 0; i < size; i++) {
-                if (storage[i].getUuid().equals(uuid)) {
-                    System.arraycopy(storage, i + 1, storage, i, size - i - 1);
-                    size--;
-                    break;
-                }
-            }
+        Resume temp = get(uuid);
+        if (temp != null) {
+            int index = Arrays.asList(storage).indexOf(temp);
+            System.arraycopy(storage, index + 1, storage, index, size - index - 1);
+            size--;
         } else {
             System.out.println("Error: there isn't resume with such uuid");
         }
