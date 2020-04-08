5a1d878491c3c92345f009a7f35efe10482eadd8
RVohmin <vohmin77@gmail.com>
2020-04-08 15:29:01 +0300

    add equals & hashcode to model

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -19,6 +19,21 @@ public class Resume {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Resume)) return false;
+
+        Resume resume = (Resume) o;
+
+        return getUuid().equals(resume.getUuid());
+    }
+
+    @Override
+    public int hashCode() {
+        return getUuid().hashCode();
+    }
+
+    @Override
     public String toString() {
         return uuid;
     }
